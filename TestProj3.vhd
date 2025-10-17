02c9d4540c35613fa1a46de1a043c33d84ad75b7
Austin Graham <austingraham731@gmail.com>
2025-10-16 23:12:18 -0700

    these files deserve to be committed

--- TestProj3.vhd
+++ TestProj3.vhd
@@ -4,13 +4,13 @@ use IEEE.STD_LOGIC_1164.ALL;
 use work.TestPackage.all;
 
 architecture sim of MyTestEntity is
-	signal TestSignal : integer := 0;
+	signal TestSignal : integer := 3;
 begin
 
 	process is
 	begin
 		some_record := c_FROM_FIFO_INIT;
-		some_other_record : t_TEST_RECORD := (some_field => 10);
+		some_other_record : t_TEST_RECORD := (some_field => 15);
 	end process;
 
   end architecture;
